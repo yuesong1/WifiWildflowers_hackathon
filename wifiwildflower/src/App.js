2037858566d45bd90056ebf6be99005cd140e227
Almaz Khalilov <almaziotheepic@gmail.com>
2023-09-26 16:25:15 +1000

    Firebase add

--- wifiwildflower/src/App.js
+++ wifiwildflower/src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const analytics = getAnalytics(app);
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => signInAnonymously(auth)}>
+          Login Anonymously
+        </button>
       </header>
     </div>
   );
