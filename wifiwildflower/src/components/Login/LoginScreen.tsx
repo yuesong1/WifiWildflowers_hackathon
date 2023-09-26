b48d2b864bf332cd4a1650179255e61b0f698102
Almaz K <34670358+Mazzz-zzz@users.noreply.github.com>
2023-09-26 17:45:40 +1000

    commiting

--- wifiwildflower/src/components/Login/LoginScreen.tsx
+++ wifiwildflower/src/components/Login/LoginScreen.tsx
@@ -12,18 +12,6 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function Copyright(props: any) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
@@ -109,7 +97,6 @@ export default function LoginScreen() {
             </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
   );
--- wifiwildflower/src/enums/RouteURLs.ts
+++
@@ -1,4 +0,0 @@
-export enum ULRs{
-    Home="/home",
-    Login="/login"
-}
