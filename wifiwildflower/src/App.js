57886d3826fa1d729de21eecd4bd0afd9fffaea8
Almaz Khalilov <almaziotheepic@gmail.com>
2023-09-26 16:56:07 +1000

    Basic login func

--- wifiwildflower/src/App.js
+++ wifiwildflower/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import { initializeApp } from "firebase/app";
-import { getAuth, signInAnonymously } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-
-console.log(process.env.REACT_APP_API_KEY)
+import Login from './screens/login.tsx';
+import { useEffect, useState } from 'react';
+import Dashboard from './screens/dashboard.tsx';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -20,18 +21,23 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const analytics = getAnalytics(app);
 
-
-
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setCurrentUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={() => signInAnonymously(auth)}>
-          Login Anonymously
-        </button>
-      </header>
+      {currentUser ? <Dashboard /> : <Login />}
     </div>
   );
 }
 
 export default App;
+
