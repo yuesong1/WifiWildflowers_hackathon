50f1d3ffba7d34a29edcf00ad248e466df9d2032
yuesong1 <yuesong1@student.unimelb.edu.au>
2023-09-27 11:39:06 +1000

    Create Camera.jsx

---
+++ wifiwildflower/src/components/Camera/Camera.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Camera = () => {
+  return (
+    <div>
+      <button> Take a photo</button>
+      <input accept="image/*" id="icon-button-file" type="file" capture="user"/>
+    </div>
+  )
+}"
+
+export default Camera
