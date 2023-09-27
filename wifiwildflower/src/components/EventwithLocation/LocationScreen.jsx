d23be07c0291a1e5e9cb0eb682d294a9b3651c08
yuesong1 <yuesong1@student.unimelb.edu.au>
2023-09-27 22:03:08 +1000

    Update LocationScreen.jsx

---
+++ wifiwildflower/src/components/EventwithLocation/LocationScreen.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { GoogleMapCard } from './GoogleMapCard';
+import { Button, Container } from '@mui/material';
+const currentPosition={
+    latitute:-37.79976696090261,
+    longitude:144.9648329121283
+}
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    console.log("Geolocation not supported");
+  }
+  
+function success(position) {
+    currentPosition.latitude = parseFloat(position.coords.latitude);
+    currentPosition.longitude =parseFloat(position.coords.longitude);
+console.log(`Latitude: ${currentPosition.latitude}, Longitude: ${currentPosition.longitude}`);
+}
+  
+function error() {
+console.log("Unable to retrieve your location");
+}
+
+const LocationScreen = props => {
+    const [mapObject, setMapObject] = useState(null);
+    return (
+    < >
+    <Container sx={{display:"flex", justifyContent:"center", flexDirection:"column"}}>
+      Your current locaiton is : {currentPosition.latitute}+{currentPosition.longitude}
+      {/* <LocatorButton mapObject={mapObject} /> */}
+      {/* <Map setMapObject={setMapObject} /> */}
+      <GoogleMapCard height="500px" lat={Number(currentPosition.latitude)} lng={Number(currentPosition.longitude)}/>
+      <Button variant="contained">Join Event</Button>
+    </Container>
+
+    </>
+  )
+}
+
+export default LocationScreen
